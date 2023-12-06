9635d84c8a249b44089d4a045fa3d7a4cb816206
Brett Higgins <brett.higgins@gmail.com>
2023-12-06 10:04:26 -0500

    Changes after running Sentry wizard for Remix
    Ran the wizard and fixed other unrelated eslint warnings (import/order).

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -9,8 +9,18 @@ import { PassThrough } from "node:stream";
 import type { EntryContext } from "@remix-run/node";
 import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
+import * as Sentry from "@sentry/remix";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
+
+export function handleError(error, { request }) {
+  Sentry.captureRemixServerException(error, "remix.server", request);
+}
+
+Sentry.init({
+  dsn: "https://dummy.ingest.sentry.io/dummy",
+  tracesSampleRate: 1,
+});
 
 const ABORT_DELAY = 5_000;
 
